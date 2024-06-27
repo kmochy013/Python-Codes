e092deea6f46c89d23efd5cf0ed9997f793845e6
kmochy013 <kmocjy013@gmail.com>
2024-06-27 12:27:49 +0600

    first commit

---
+++ practice/print.py
@@ -0,0 +1,7 @@
+# "print" diye python e kono jinish er output dekhano hoi.
+
+# Double Quatation ("") needed to print any words and charecters
+print("First Day of Python Programme 27th June, 2024 ") 
+
+# Double Quatation ("") not needed to print any Numbers.
+print(420)
---
+++ practice/variable.py
@@ -0,0 +1,5 @@
+# 'name' is a variable name and in the double qutation we store what we need to print.
+name = "2556565"
+
+# To call any variable in print function we dont need any qutation
+print(name)
