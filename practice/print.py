9b8f92621b84b8cdecb8ef408bcfd7046d1cfd24
kmochy013 <kmocjy013@gmail.com>
2024-06-27 12:30:05 +0600

    commited

--- practice/print.py
+++ practice/print.py
@@ -2,6 +2,7 @@
 
 # Double Quatation ("") needed to print any words and charecters
 print("First Day of Python Programme 27th June, 2024 ") 
+print("Welcome to Xorvik's Python World")
 
 # Double Quatation ("") not needed to print any Numbers.
 print(420)
